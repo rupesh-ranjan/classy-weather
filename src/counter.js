10c714d36d3ba083707eebcf2175a2bbe6cebae8
Rupesh Ranjan <2068799@cognizant.com>
2024-02-27 16:37:24 +0530

    date counter app

---
+++ src/counter.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0,
+    };
+    this.handleDecrement = this.handleDecrement.bind(this);
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+  handleDecrement() {
+    this.setState((currentState) => {
+      return { count: currentState.count - 1 };
+    });
+    // this.setState({ count: 10 });
+  }
+
+  handleIncrement() {
+    this.setState((currentState) => {
+      return { count: currentState.count + 1 };
+    });
+  }
+  render() {
+    const date = new Date("Feb 27 2024");
+    date.setDate(date.getDate() + this.state.count);
+
+    return (
+      <div>
+        <button onClick={this.handleDecrement}>-</button>
+        <span>
+          {date.toDateString()}[{this.state.count}]
+        </span>
+        <button onClick={this.handleIncrement}>+</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
